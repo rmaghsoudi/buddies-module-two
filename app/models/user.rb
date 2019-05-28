72c88b6f3f3acbcd5f2ba430ca667bdf83317019
Paula Tulis <tulis.paula@gmail.com>
2019-05-28 15:10:47 -0400

    added tutors method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ActiveRecord::Base
   has_many :sessions
   has_many :subjects, through: :sessions
-  
+
   def full_name
     self.first_name + " " + self.last_name
   end
+
+  def self.tutors
+    self.all.select { |user| user.tutor }
+  #   self.all.map do |x|
+  #   if x.tutor
+  #     return x.name
+  #   end
+  # end
+end
+
 end
