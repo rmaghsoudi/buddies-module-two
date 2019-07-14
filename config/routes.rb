eeb81a661defc1efcf8c6f4625b1be76d29eb574
rmaghsoudi <romy_mag@outlook.com>
2019-07-14 10:17:30 -0400

    again

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'users#home'
+  root 'users#home'
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
